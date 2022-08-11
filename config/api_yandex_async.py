ff5073351c24fa0e9fe31ab745c3b2823f8f23c3
Sebastianbipbip <maks.ard99@gmail.com>
2022-08-12 00:33:19 +0300

    fix

--- config/api_yandex_async.py
+++ config/api_yandex_async.py
@@ -1,6 +1,4 @@
 """
-ID: 1268a5e054fd4fbdbc4336a92cbbeb79
-Пароль: 2f3f1516fb3a40dc9365f7f798c11ae6
 Callback URL: https://oauth.yandex.ru/verification_code
 Время жизни токена: Не менее, чем 1 год
 Дата создания: 29.03.2022
@@ -10,8 +8,8 @@ import aiohttp
 import requests
 
 from progress.bar import IncrementalBar
+from config.privat_info import TOKEN
 
-TOKEN = "AQAAAABaprzMAAfIkr1IL8KgfEqppqmoi30MEUs"
 headers = {'Authorization': f'OAuth {TOKEN}'}
 metrics = {}
 count = 0
--- config/editor.py
+++ config/editor.py
@@ -1,6 +1,10 @@
 """Редактирует файл с id, если что то меняется в таблице"""
+"""В процессе"""
+import json
 import openpyxl
 import pandas as pd
+from datetime import datetime
+from calendar import monthrange
 
 
 class GetIdRow:
@@ -20,22 +24,50 @@ class GetIdRow:
         return [i for i in self.get_all_name_column("A") if i[1][-1] != ':']
 
     def get_id_row(self):
+        """Получает id целей"""
         return {item[1]: item[0] for item in self.get_all_name_column("BR")}
 
-    def update_date(self, month):
+    def copy_list(self, title=None):
+        """Копирует лист для нового месяца"""
+        if title is None:
+            name_sheet = json.load(open(r"data/name_sheet.json", encoding="utf-8"))
+            month_now = datetime.now().strftime("%m")
+            title = name_sheet[month_now]  # Название устанавливается согласно текущему месяцу
+
+        new_sheet = self.book.copy_worksheet(self.sheet)
+        new_sheet.title = title
+        self.update_date(new_sheet)
+
+        self.book.save(self.filename)
+
+    def update_date(self, new_sheet, year: str = None, month: str = None):
+        """Обновляет строки с датой на актуальные для листа
+        :param new_sheet: лист, где надо обновить даты
+        :param year: str год формата yyyy
+        :param month: str месяц формата mm"""
+
+        if month is None:
+            month = datetime.now().strftime("%m")
+        if year is None:
+            year = datetime.now().year
+
+        days = monthrange(year, int(month))[1]
+
         month_limit = {
             29: 71,
             30: 73,
             31: 75
         }
-
-        result = [chr(i) for i in range(66, 91, 2)] + ['B' + chr(i) for i in range(66, month_limit[month], 2)]
-        daterange = pd.date_range("2022-08-01", "2022-08-31").strftime("%d.%m.20%y")
+        result = [chr(i) for i in range(66, 91, 2)] + \
+                 ['A' + chr(i) for i in range(66, 91, 2)] + \
+                 ['B' + chr(i) for i in range(66, month_limit[days], 2)]
+        daterange = pd.date_range(f"{year}-{month}-01", f"{year}-{month}-{days}").strftime("%d.%m.20%y")
 
         day = 0
         for item in result:
-            self.sheet[item + "1"] = daterange[day]
-            self.sheet[item + "67"] = daterange[day]
+            new_sheet[item + "1"] = daterange[day]
+            new_sheet[item + "67"] = daterange[day]
 
             day += 1
+
         self.book.save(self.filename)
